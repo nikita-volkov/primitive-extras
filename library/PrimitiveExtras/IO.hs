d1db17e9bc70d306bb9e64a283d7bb960ac2739d
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-07-11 18:25:59 +0300

    Add generateArray

--- library/PrimitiveExtras/IO.hs
+++ library/PrimitiveExtras/IO.hs
@@ -11,3 +11,17 @@ generateUnliftedArray = A.generate
 
 replicateUnliftedArray :: PrimUnlifted a => Int -> IO a -> IO (UnliftedArray a)
 replicateUnliftedArray = A.replicateIO
+
+generateArray :: Int -> (Int -> IO a) -> IO (Array a)
+generateArray size elementIO =
+  do
+    array <- newArray size undefined
+    let
+      loop index =
+        if index < size
+          then do
+            element <- elementIO index
+            writeArray array index element
+            loop (succ index)
+          else unsafeFreezeArray array
+      in loop 0
