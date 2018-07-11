9005a11b3c45bcd4a70c7ec1e449023d8b1cb5f3
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-07-11 16:55:38 +0300

    Add UnfoldM

---
+++ library/PrimitiveExtras/UnfoldM.hs
@@ -0,0 +1,20 @@
+module PrimitiveExtras.UnfoldM
+where
+
+import PrimitiveExtras.Prelude hiding (fold)
+import PrimitiveExtras.Types
+import DeferredFolds.UnfoldM
+import qualified PrimitiveExtras.Fold as A
+import qualified PrimitiveExtras.UnliftedArray as B
+
+
+primMultiArrayIndices :: Monad m => PrimMultiArray a -> UnfoldM m Int
+primMultiArrayIndices (PrimMultiArray ua) =
+  intsInRange 0 (pred (sizeofUnliftedArray ua))
+
+primMultiArrayAt :: (Monad m, Prim prim) => PrimMultiArray prim -> Int -> UnfoldM m prim
+primMultiArrayAt (PrimMultiArray ua) index =
+  B.at ua index empty primArray
+
+primArray :: (Monad m, Prim prim) => PrimArray prim -> UnfoldM m prim
+primArray ba = UnfoldM $ \f z -> foldlPrimArrayM' f z ba
