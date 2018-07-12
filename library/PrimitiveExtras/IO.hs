ae0df1f2f72115fdd10e66b3cdceafcd1a01fe06
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-07-12 12:49:23 +0300

    Add concurrent traversal tools

--- library/PrimitiveExtras/IO.hs
+++ library/PrimitiveExtras/IO.hs
@@ -67,3 +67,43 @@ replicatePrimArray size elementIO =
             loop (succ index)
           else unsafeFreezePrimArray array
       in loop 0
+
+traversePrimArrayWithIndexInRange :: Prim a => PrimArray a -> Int -> Int -> (Int -> a -> IO ()) -> IO ()
+traversePrimArrayWithIndexInRange primArray from to action =
+  let iterate index = if index < to
+        then do
+          action index $! indexPrimArray primArray index
+          iterate (succ index)
+        else return ()
+      in iterate from
+
+traversePrimArrayWithIndexConcurrently ::
+  Prim a =>
+  PrimArray a ->
+  Int ->
+  (Int -> a -> IO ()) ->
+  IO (IO ()) {-^ An action, which blocks until the concurrent computation is finished. -}
+traversePrimArrayWithIndexConcurrently primArray threadsAmount action =
+  let size = sizeofPrimArray primArray
+      maxIndex = pred size
+      step = div size threadsAmount
+      in case step of
+        0 -> itraversePrimArray_ action primArray $> return ()
+        _ -> do
+          semaphore <- newTVarIO threadsAmount
+          let
+            startThreads index = if index >= size
+              then return ()
+              else let
+                !nextIndex = step + index
+                in do
+                  forkIO $ do
+                    traversePrimArrayWithIndexInRange primArray index (min nextIndex size) action
+                    atomically (modifyTVar' semaphore pred)
+                  startThreads nextIndex
+            in do
+              startThreads 0
+              return $ atomically $ do
+                activeThreads <- readTVar semaphore
+                guard (activeThreads == 0)
+
