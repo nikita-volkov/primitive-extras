b49fd5c04500d3c9f346f0500d3c9778ab5ae4e8
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-07-12 21:28:22 +0300

    Add oneHotPrimArray

--- library/PrimitiveExtras/Pure.hs
+++ library/PrimitiveExtras/Pure.hs
@@ -9,3 +9,10 @@ import qualified PrimitiveExtras.UnliftedArray as A
 {-| Get length of the outer dimension of a primitive multi array -}
 primMultiArrayOuterLength :: PrimMultiArray a -> Int
 primMultiArrayOuterLength (PrimMultiArray outerDimension) = sizeofUnliftedArray outerDimension
+
+oneHotPrimArray :: Prim a => Int {-^ Size -} -> Int {-^ Index -} -> a -> PrimArray a
+oneHotPrimArray size index value =
+  runST $ do
+    marr <- newPrimArray size
+    writePrimArray marr index value
+    unsafeFreezePrimArray marr
