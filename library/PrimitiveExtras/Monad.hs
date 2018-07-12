f5cd709f4c0d22285fed6ac0e0f20a054d3bf646
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-07-12 23:36:38 +0300

    Add replicateMPrimArray

--- library/PrimitiveExtras/Monad.hs
+++ library/PrimitiveExtras/Monad.hs
@@ -13,3 +13,18 @@ primMultiArray outerArraySize runFold =
   do
     indexCounts <- runFold (lmap fst (A.indexCounts outerArraySize))
     runFold (A.primMultiArray (indexCounts :: PrimArray Word32))
+
+{-| Please notice that this function is highly untested -}
+replicateMPrimArray :: (Monad m, Prim element) => Int -> m element -> m (PrimArray element)
+replicateMPrimArray size elementM =
+  do
+    !mutable <- return (unsafeDupablePerformIO (newPrimArray size))
+    let 
+      iterate index =
+        if index < size
+          then do
+            element <- elementM
+            let !() = unsafeDupablePerformIO (writePrimArray mutable index element)
+            iterate (succ index)
+          else return (unsafePerformIO (unsafeFreezePrimArray mutable))
+      in iterate 0
