dc649aa737c5930275c38249efe98d1e9c1862d0
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-07-13 15:21:48 +0300

    Add Ord and Show instances

--- library/PrimitiveExtras/Instances.hs
+++ library/PrimitiveExtras/Instances.hs
@@ -5,3 +5,7 @@ import PrimitiveExtras.Prelude
 import PrimitiveExtras.Types
 
 deriving instance (Eq a, Prim a) => Eq (PrimMultiArray a)
+
+deriving instance (Ord a, Prim a) => Ord (PrimMultiArray a)
+
+deriving instance (Show a, Prim a) => Show (PrimMultiArray a)
